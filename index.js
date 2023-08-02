18a15151dc3d1b7df278d541f62e9e4cd20a0b3a
MICHELKAJI <121295124+MICHELKAJI@users.noreply.github.com>
2023-08-02 17:37:33 +0000

    add message motivation

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function message(message){
     const blague =["vous avez un gros nez","vous avez une salle tête","vous devrez consulter un psy","vous avez des grosses joux"];
     const astrologique =["Tu seras de bonne humeur aujourd'hui","cette journnée tu vas ganger à loto","vous allez trouver le grand amour","vous allez décroché un bon boulot","l'avenire sera bon pour vous"];
-    const motivation = ["un gagneur ne lâche","un homme prudent voit le mal de loin et s'est cache","la sagesse n'a pas d'âge","un homme patient monge le fruit mur","l'humilité precede la gloire"];
+    const motivation = ["un gagneur ne lâche","un homme prudent voit le mal de loin et s'est cache","la sagesse n'a pas d'âge","un homme patient monge le fruit mur","l'humilité precede la gloire","reste toi"];
     
     if(message === 'blague'){
         const nb = Math.floor(Math.random()*blague.length);
